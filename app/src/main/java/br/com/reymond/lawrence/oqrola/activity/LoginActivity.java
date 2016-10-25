a4b8915095ff35eba183e3e6666c7c894b63f5cd
ian.reymond <lawrence.reymond@gmail.com>
2016-10-25 00:01:34 -0200

    Inserindo Firebase parte 2.2.2 Organizacao do projeto ( Adicionando Pacotes: activity, model e util)

---
+++ app/src/main/java/br/com/reymond/lawrence/oqrola/activity/LoginActivity.java
@@ -0,0 +1,69 @@
+package br.com.reymond.lawrence.oqrola.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import br.com.reymond.lawrence.oqrola.R;
+
+/**
+ * Created by ian on 24-Oct-16.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText txtUser;
+    private EditText txtPassword;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        txtUser = (EditText) findViewById(R.id.txtUser);
+        txtPassword = (EditText) findViewById(R.id.txtPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signInWithEmailAndPassword(txtUser.getText().toString(), txtPassword.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (!task.isSuccessful()) {
+                                    Toast.makeText(LoginActivity.this, R.string.error_login, Toast.LENGTH_LONG).show();
+                                } else {
+                                    FirebaseUser user = task.getResult().getUser();
+                                    if (user != null) {
+                                        Intent it = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(it);
+                                        finish();
+                                    }
+                                }
+                            }
+                        });
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+}
--- app/src/main/java/br/com/reymond/lawrence/oqrola/activity/MainActivity.java
+++ app/src/main/java/br/com/reymond/lawrence/oqrola/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.reymond.lawrence.oqrola;
+package br.com.reymond.lawrence.oqrola.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.reymond.lawrence.oqrola.R;
 
 public class MainActivity extends AppCompatActivity {
 
