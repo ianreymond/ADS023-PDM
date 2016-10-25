488f843c1244eead1e2dac5a03521f1608e47cdd
ian.reymond <lawrence.reymond@gmail.com>
2016-10-25 11:40:03 -0200

    Inserindo Firebase parte 2.3.1 Ajustes de Codigo - Primeira Execução Sem erros

--- app/src/main/java/br/com/reymond/lawrence/oqrola/activity/LoginActivity.java
+++ app/src/main/java/br/com/reymond/lawrence/oqrola/activity/LoginActivity.java
@@ -24,6 +24,7 @@ import br.com.reymond.lawrence.oqrola.R;
  * Created by ian on 24-Oct-16.
  */
 
+
 public class LoginActivity extends AppCompatActivity {
     private EditText txtUser;
     private EditText txtPassword;
