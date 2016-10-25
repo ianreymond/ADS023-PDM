e48bd76fe98a3cecc96a4ab2548e61b55ea5d5e6
ian.reymond <lawrence.reymond@gmail.com>
2016-10-24 23:53:47 -0200

    Inserindo Firebase parte 2.2

---
+++ app/src/main/java/br/com/reymond/lawrence/oqrola/LoginActivity.java
@@ -0,0 +1,12 @@
+package br.com.reymond.lawrence.oqrola;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ian on 24-Oct-16.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+
+
+}
